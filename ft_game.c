91f566829690cc4f2d513e7c32b745bdfe90b0bf
Your Name <you@example.com>
2023-12-17 08:18:37 +0900

    終了処理を見直し

--- ft_game.c
+++ ft_game.c
@@ -6,26 +6,27 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 12:33:02 by anonymous         #+#    #+#             */
-/*   Updated: 2023/12/03 15:18:46 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/12/17 08:17:40 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	ft_game_initialize(t_game *game, const char *map_file)
+#include <stdlib.h>
+#include <unistd.h>
+
+void	ft_game_initialize(t_game *game, const char *map_file)
 {
-	ft_bzero(game, sizeof(t_game));
-	if (ft_map_read(game, map_file) == FALSE)
-		return (FALSE);
-	if (ft_map_validate(game) == FALSE)
-	{
-		ft_putendl_fd(game->message, 1);
-		return (FALSE);
-	}
-	return (TRUE);
+	ft_map_read(game, map_file);
+	ft_map_validate(game);
 }
 
-void	ft_game_finalize(t_game *game)
+void	ft_game_finalize(t_game *game, char *error)
 {
 	ft_map_free(game);
+	if (error == NULL)
+		exit(0);
+	ft_putendl_fd("Error", STDERR_FILENO);
+	ft_putendl_fd(error, STDERR_FILENO);
+	exit(1);
 }
