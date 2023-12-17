5f4e07d80034aad28691eb006a63b92687b5bd04
Your Name <you@example.com>
2023-12-17 09:59:02 +0900

    終了処理を見直し

--- ft_map.c
+++ ft_map.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 12:36:24 by anonymous         #+#    #+#             */
-/*   Updated: 2023/12/03 15:42:45 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/12/17 09:55:08 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,67 +15,77 @@
 #include <unistd.h>
 #include "so_long.h"
 
-static int	add_line(t_game *game, char *line)
-{
-	char	**new_map;
-	char	*ptr;
-
-	ptr = ft_strchr(line, '\n');
-	if (ptr == NULL)
-		return (FALSE);
-	new_map = (char **)ft_calloc(game->height + 1, sizeof(char *));
-	if (new_map == NULL)
-		return (FALSE);
-	ft_memcpy(new_map, game->map, game->height * sizeof(char *));
-	new_map[game->height] = line;
-	*ptr = '\0';
-	free(game->map);
-	game->map = new_map;
-	game->height += 1;
-	return (TRUE);
-}
-
-int	ft_map_read(t_game *game, const char *map_file)
+static void	check_characters(t_game *game, const char *map_file)
 {
 	int		fd;
-	char	*line;
+	ssize_t	n;
+	char	c;
 
 	fd = open(map_file, O_RDONLY);
 	if (fd == -1)
-		return (FALSE);
-	game->status = TRUE;
+		ft_game_finalize(game, "地図ファイルがオープンできませんでした。");
+	c = '\n';
+	n = read(fd, &c, sizeof(char));
+	while (n > 0)
+	{
+		if (c == '\n')
+			game->height++;
+		else if (c == '\0' || ft_strchr("01CEP", c) == NULL)
+		{
+			close(fd);
+			ft_game_finalize(game, "地図ファイルに不正な文字が含まれています。");
+		}
+		n = read(fd, &c, sizeof(char));
+	}
+	close(fd);
+	if (n < 0)
+		ft_game_finalize(game, "地図ファイルの読み込み中にエラーが発生しました。");
+	else if (c != '\n')
+		game->height++;
+}
+
+void	ft_map_read(t_game *game, const char *map_file)
+{
+	int		fd;
+	char	**row;
+	char	*line;
+	size_t	len;
+
+	check_characters(game, map_file);
+	game->map = (char **)ft_calloc(game->height + 1, sizeof(char *));
+	if (game->map == NULL)
+		ft_game_finalize(game, "地図データのメモリ確保に失敗しました。");
+	fd = open(map_file, O_RDONLY);
+	if (fd == -1)
+		ft_game_finalize(game, "地図ファイルがオープンできませんでした。");
+	row = game->map;
 	line = get_next_line(fd);
 	while (line != NULL)
 	{
-		if (game->status == TRUE)
-			game->status = add_line(game, line);
-		if (game->status == FALSE)
-			free(line);
+		len = ft_strlen(line);
+		if (line[len - 1] == '\n')
+			line[len - 1] = '\0';
+		*row++ = line;
 		line = get_next_line(fd);
 	}
 	close(fd);
-	if (game->status == FALSE)
-		ft_map_free(game);
-	return (game->status);
+}
+
+void	ft_map_validate(t_game *game)
+{
+	ft_validation_is_rectangular(game);
+	ft_validation_is_surrounded(game);
 }
 
 void	ft_map_free(t_game *game)
 {
-	int	i;
+	char	**ptr;
 
-	i = 0;
-	while (i < game->height)
-		free(game->map[i++]);
+	if (game->map == NULL)
+		return ;
+	ptr = game->map;
+	while (*ptr)
+		free(*ptr++);
 	free(game->map);
 	game->map = NULL;
-}
-
-int	ft_map_validate(t_game *game)
-{
-	if (ft_validation_is_rectangular(game) == FALSE)
-	{
-		ft_strlcpy(game->message, "地図が長方形ではありません。", sizeof(game->message));
-		return (FALSE);
-	}
-	return (TRUE);
 }
