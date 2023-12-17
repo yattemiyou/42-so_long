c233f72acbf0a72eaa3e9e28f8c52aebeb7a4b2a
Your Name <you@example.com>
2023-12-17 09:53:21 +0900

    終了処理を見直し

--- ft_validation.c
+++ ft_validation.c
@@ -6,26 +6,49 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 15:40:51 by anonymous         #+#    #+#             */
-/*   Updated: 2023/12/03 15:41:10 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/12/17 09:51:09 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	ft_validation_is_rectangular(t_game *game)
+void	ft_validation_is_rectangular(t_game *game)
 {
-	int	i;
+	size_t	i;
 
 	if (game->height == 0)
-		return (FALSE);
+		ft_game_finalize(game, "高さが0です。");
 	game->width = ft_strlen(game->map[0]);
 	if (game->width == 0)
-		return (FALSE);
+		ft_game_finalize(game, "幅が0です。");
 	i = 0;
 	while (i < game->height)
 	{
-		if (ft_strlen(game->map[i++]) != (size_t)game->width)
-			return (FALSE);
+		if (ft_strlen(game->map[i++]) != game->width)
+			ft_game_finalize(game, "長方形ではありません。");
 	}
-	return (TRUE);
+}
+
+void	ft_validation_is_surrounded(t_game *game)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < game->width)
+	{
+		if (game->map[0][i] != '1')
+			ft_game_finalize(game, "壁で囲まれていません。");
+		if (game->map[game->height - 1][i] != '1')
+			ft_game_finalize(game, "壁で囲まれていません。");
+		i++;
+	}
+	i = 0;
+	while (i < game->height)
+	{
+		if (game->map[i][0] != '1')
+			ft_game_finalize(game, "壁で囲まれていません。");
+		if (game->map[i][game->width - 1] != '1')
+			ft_game_finalize(game, "壁で囲まれていません。");
+		i++;
+	}
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:42:49 by anonymous         #+#    #+#             */
-/*   Updated: 2023/12/03 15:39:54 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/12/17 09:52:46 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,21 @@
 typedef struct s_game
 {
 	char	**map;
-	int		width;
-	int		height;
-	int		status;
-	char	message[256];
+	size_t	width;
+	size_t	height;
+	size_t	player;
+	size_t	exit;
+	size_t	collectible;
 }	t_game;
 
-int		ft_game_initialize(t_game *game, const char *map_file);
-void	ft_game_finalize(t_game *game);
+void	ft_game_initialize(t_game *game, const char *map_file);
+void	ft_game_finalize(t_game *game, char *error);
 
-int		ft_map_read(t_game *game, const char *map_file);
+void	ft_map_read(t_game *game, const char *map_file);
+void	ft_map_validate(t_game *game);
 void	ft_map_free(t_game *game);
-int		ft_map_validate(t_game *game);
 
-int		ft_validation_is_rectangular(t_game *game);
+void	ft_validation_is_rectangular(t_game *game);
+void	ft_validation_is_surrounded(t_game *game);
 
 #endif
