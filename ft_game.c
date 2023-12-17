08ab1169b0d049e192cc12fde6dd503080a091af
Your Name <you@example.com>
2023-12-17 10:31:34 +0900

    拡張子のチェック

--- ft_game.c
+++ ft_game.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 12:33:02 by anonymous         #+#    #+#             */
-/*   Updated: 2023/12/17 08:17:40 by anonymous        ###   ########.fr       */
+/*   Updated: 2023/12/17 10:30:35 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define EXT ".ber"
+
 void	ft_game_initialize(t_game *game, const char *map_file)
 {
+	const char	*ptr;
+
+	if (ft_strlen(map_file) <= ft_strlen(EXT))
+		ft_game_finalize(game, "ファイル名が不正です。");
+	ptr = map_file + (ft_strlen(map_file) - ft_strlen(EXT));
+	if (ft_strncmp(ptr, EXT, ft_strlen(EXT)) != 0)
+		ft_game_finalize(game, "ファイル名が不正です。");
 	ft_map_read(game, map_file);
 	ft_map_validate(game);
 }
